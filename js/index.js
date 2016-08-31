80fcaacccff6eccf036f6af03a109eebc9dbbf8c
Glauber Modolo Cabral <glauber.sp@gmail.com>
2016-08-31 09:24:40 -0300

    Relatório completo
    Finalização do relatório para avaliação.

--- js/index.js
+++ js/index.js
@@ -13,85 +13,87 @@
 
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
-( function( window ) {
-'use strict';
-function classReg( className ) {
-  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-}
-var hasClass, addClass, removeClass;
+(function(window) {
+    'use strict';
 
-if ( 'classList' in document.documentElement ) {
-  hasClass = function( elem, c ) {
-    return elem.classList.contains( c );
-  };
-  addClass = function( elem, c ) {
-    elem.classList.add( c );
-  };
-  removeClass = function( elem, c ) {
-    elem.classList.remove( c );
-  };
-}
-else {
-  hasClass = function( elem, c ) {
-    return classReg( c ).test( elem.className );
-  };
-  addClass = function( elem, c ) {
-    if ( !hasClass( elem, c ) ) {
-      elem.className = elem.className + ' ' + c;
+    function classReg(className) {
+        return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
     }
-  };
-  removeClass = function( elem, c ) {
-    elem.className = elem.className.replace( classReg( c ), ' ' );
-  };
-}
+    var hasClass, addClass, removeClass;
 
-function toggleClass( elem, c ) {
-  var fn = hasClass( elem, c ) ? removeClass : addClass;
-  fn( elem, c );
-}
-var classie = {
-  hasClass: hasClass,
-  addClass: addClass,
-  removeClass: removeClass,
-  toggleClass: toggleClass,
-  has: hasClass,
-  add: addClass,
-  remove: removeClass,
-  toggle: toggleClass
-};
-if ( typeof define === 'function' && define.amd ) {
-  define( classie );
-} else {
-  window.classie = classie;
-}
-})( window );
-var $ = function(selector){
-  return document.querySelector(selector);
+    if ('classList' in document.documentElement) {
+        hasClass = function(elem, c) {
+            return elem.classList.contains(c);
+        };
+        addClass = function(elem, c) {
+            elem.classList.add(c);
+        };
+        removeClass = function(elem, c) {
+            elem.classList.remove(c);
+        };
+    } else {
+        hasClass = function(elem, c) {
+            return classReg(c).test(elem.className);
+        };
+        addClass = function(elem, c) {
+            if (!hasClass(elem, c)) {
+                elem.className = elem.className + ' ' + c;
+            }
+        };
+        removeClass = function(elem, c) {
+            elem.className = elem.className.replace(classReg(c), ' ');
+        };
+    }
+
+    function toggleClass(elem, c) {
+        var fn = hasClass(elem, c) ? removeClass : addClass;
+        fn(elem, c);
+    }
+    var classie = {
+        hasClass: hasClass,
+        addClass: addClass,
+        removeClass: removeClass,
+        toggleClass: toggleClass,
+        has: hasClass,
+        add: addClass,
+        remove: removeClass,
+        toggle: toggleClass
+    };
+    if (typeof define === 'function' && define.amd) {
+        define(classie);
+    } else {
+        window.classie = classie;
+    }
+})(window);
+var $ = function(selector) {
+    'use strict';
+    return document.querySelector(selector);
 }
 var accordion = $('.accordion');
-accordion.addEventListener("click",function(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  if(e.target && e.target.nodeName == "A") {
-    var classes = e.target.className.split(" ");
-    if(classes) {
-      for(var x = 0; x < classes.length; x++) {
-        if(classes[x] == "accordionTitle") {
-          var title = e.target;
-          var content = e.target.parentNode.nextElementSibling;
-          classie.toggle(title, 'accordionTitleActive');
-          if(classie.has(content, 'accordionItemCollapsed')) {
-            if(classie.has(content, 'animateOut')){
-              classie.remove(content, 'animateOut');
+accordion.addEventListener("click", function(e) {
+    'use strict';
+    e.stopPropagation();
+    e.preventDefault();
+    if (e.target && e.target.nodeName == "A") {
+        var classes = e.target.className.split(" ");
+        if (classes) {
+            for (var x = 0; x < classes.length; x++) {
+                if (classes[x] == "accordionTitle") {
+                    var title = e.target;
+                    var content = e.target.parentNode.nextElementSibling;
+                    classie.toggle(title, 'accordionTitleActive');
+                    if (classie.has(content, 'accordionItemCollapsed')) {
+                        if (classie.has(content, 'animateOut')) {
+                            classie.remove(content, 'animateOut');
+                        }
+                        classie.add(content, 'animateIn');
+                    } else {
+                        classie.remove(content, 'animateIn');
+                        classie.add(content, 'animateOut');
+                    }
+                    classie.toggle(content, 'accordionItemCollapsed');
+                }
             }
-            classie.add(content, 'animateIn');
-          }else{
-             classie.remove(content, 'animateIn');
-             classie.add(content, 'animateOut');
-          }
-          classie.toggle(content, 'accordionItemCollapsed');      
         }
-      }
-    }  
-  }
+    }
 });
